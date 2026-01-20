19e0157e58c9d2165040ff6d0ec2f0d674a3dbd1
alw4ys-on <felipe12072007@gmail.com>
2026-01-20 19:43:05 -0300

    Versão 0.1a

---
+++ helloworld.js
@@ -0,0 +1,6 @@
+let http = require('http');
+
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmve3LlOi0V3sXXfZczxWmm5M4xNb0BvotIQ&s" alt="abacaxi bolado">');
+}).listen(8888);
