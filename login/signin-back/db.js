952f3427f03644e4282541de336461dd31b96b6f
alw4ys-on <felipe12072007@gmail.com>
2026-01-21 20:11:27 -0300

    v0.1.2a_pre-alpha

--- login/signin-back/db.js
+++ login/signin-back/db.js
@@ -1,1 +1,14 @@
-let sql = require('sqlite3');
+// dor de cabeça
+
+const sql = require('mysql');
+
+let con = sql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
